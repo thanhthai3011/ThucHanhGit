cef4c217c4947dde4a5633e4524f3219247e4f86
Trung Thanh <letrungthanh101@gmail.com>
2021-04-03 10:53:45 +0700

    Thanh add component pass and add actionlistener

--- src/thuchanhgithub/LoginForm.java
+++ src/thuchanhgithub/LoginForm.java
@@ -13,6 +13,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 public class LoginForm extends JFrame implements ActionListener {
 
     /**
@@ -20,12 +21,13 @@ public class LoginForm extends JFrame implements ActionListener {
      */
     Container container = getContentPane();
     JLabel userLabel = new JLabel("USERNAME");
-    //
+    JLabel passwordLabel = new JLabel("PASSWORD");
     JTextField userTextField = new JTextField();
-    //
+    JPasswordField passwordField = new JPasswordField();
     JButton loginButton = new JButton("LOGIN");
     JButton resetButton = new JButton("RESET");
     JCheckBox showPassword = new JCheckBox("Show Password");
+
     LoginForm() {
         setLayoutManager();
         setLocationAndSize();
@@ -39,25 +41,59 @@ public class LoginForm extends JFrame implements ActionListener {
 
     public void setLocationAndSize() {
         userLabel.setBounds(50, 150, 100, 30);
-        //
+        passwordLabel.setBounds(50, 220, 100, 30);
         userTextField.setBounds(150, 150, 150, 30);
-        //
+        passwordField.setBounds(150, 220, 150, 30);
         showPassword.setBounds(150, 250, 150, 30);
         loginButton.setBounds(50, 300, 100, 30);
         resetButton.setBounds(200, 300, 100, 30);
 
-
     }
-    
+
     public void addComponentsToContainer() {
         container.add(userLabel);
-        //pass
+        container.add(passwordLabel);
         container.add(userTextField);
-        //pass
+        container.add(passwordField);
         container.add(showPassword);
         container.add(loginButton);
         container.add(resetButton);
     }
+
+    public void addActionEvent() {
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        showPassword.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginButton) {
+            String userText;
+            String pwdText;
+            userText = userTextField.getText();
+            pwdText = passwordField.getText();
+            if (userText.equalsIgnoreCase("admin") && pwdText.equalsIgnoreCase("12345")) {
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+
+        }
+        if (e.getSource() == resetButton) {
+            userTextField.setText("");
+            passwordField.setText("");
+        }
+        if (e.getSource() == showPassword) {
+            if (showPassword.isSelected()) {
+                passwordField.setEchoChar((char) 0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
+
+        }
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
